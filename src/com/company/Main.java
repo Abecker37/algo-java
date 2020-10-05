60dc0149fb72504365dd6a2e587dd7a078676540
Becker Amaury <amaury.becker@gmail.com>
2020-10-05 16:55:53 +0200

    tp tableau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,32 @@ package com.company;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
 	// write your code here
+        int[] scores= new int [7];
+        scores[0]=1;
+        scores[1]=2;
+        scores[2]=3;
+        scores[3]=4;
+        scores[4]=5;
+        scores[5]=6;
+        scores[6]=7;
+        int moyenne=0;
+        for(int i=0;i<scores.length; i++) {
+            int score1 = scores[i];
+            System.out.println(scores + " ");
+        }
+        System.out.println();
+            for(int i=0;i<scores.length; i++){
+                moyenne =moyenne+scores[i];
+
+            }
+
+            moyenne=moyenne/scores.length;
+        System.out.println("moyenne "+moyenne);
+
+        }
     }
-}
-    
+
